301a827796c574ea80e7df5052ce1349ea993344
crimson206 <sisung.kim1@gmail.com>
2024-07-20 15:04:50 +0200

    added integration_test abstraction

---
+++ src/crimson/microwise_development/abstraction/test_manager/__init__.py
@@ -0,0 +1,6 @@
+description = """
+In traditional Development environment, the influence of
+a modification of a module on other modules can be immediately tested.
+In Micro-wise Development, micro modules are stored separately, therefore we need a special setup
+for the integration and e2e tests over different repositories.
+"""
