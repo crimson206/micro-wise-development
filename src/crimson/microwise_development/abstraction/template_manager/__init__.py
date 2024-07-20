ba1e1e9fa8de7d9261f2674fb1f826a189e4b871
crimson206 <sisung.kim1@gmail.com>
2024-07-20 10:11:42 +0200

    added requirement abstraction

---
+++ src/crimson/microwise_development/abstraction/template_manager/__init__.py
@@ -0,0 +1,5 @@
+"""
+In Micro-wise Development, we need to set the development environment very often.
+Therefore, we need an environment provider. The environment can be set by different files.
+This template_provider module must play this role.
+"""
